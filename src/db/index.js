dcf3582e0ea390d100ea99637b5800175ae290e0
kronosvamsi <beingaliver@gmail.com>
2025-06-22 16:00:17 +0530

    mongodb connection

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async() => {
+
+    try {
+
+        let connectionInstance= await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+
+        console.log("mongodb connected instance :",connectionInstance.connection.host)
+        
+    } catch (error) {
+        console.log("Mongo DB connection failed :",error);
+        process.exit(1);
+        
+    }
+
+}
+
+export default connectDB;
